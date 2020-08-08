e30c31d112db87befa1e7694abf3021f6afdc5e6
Felipe Genuino <felipe.genuino@softplan.com.br>
2020-08-08 02:42:42 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7tYVkx9yCjcCVps8KDuLUkAbn7sXEg34",
+  authDomain: "clone-e9a48.firebaseapp.com",
+  databaseURL: "https://clone-e9a48.firebaseio.com",
+  projectId: "clone-e9a48",
+  storageBucket: "clone-e9a48.appspot.com",
+  messagingSenderId: "576102464431",
+  appId: "1:576102464431:web:334a8fbea214d8b10c0d5a",
+  measurementId: "G-X1WLQ5CZQ3",
+};
